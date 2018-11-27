580c0cecc0302a7f72772c51be8e4dfbe5daae91
BT_1N3_32 <lpan7932@bths.edu>
2018-11-27 15:13:09 -0500

    Final Commit

--- src/JackOLanturn.java
+++ src/JackOLanturn.java
@@ -5,8 +5,34 @@ public class JackOLanturn
     {
         this.face = face;
     }
-    public static void fill (String str)
+    public  void fill (String str)
     {
-        //nested for loop
+        for(int i = 0; i < face.length; i++)
+        {
+            for( int x = 0; x < face[i].length; x++)
+            {
+                face[i][x] = (str);
+            }
+        }
+    }
+
+    public void edit(String replace, int row, int column)
+    {
+        face[row][column] = replace;
+    }
+
+    @Override
+    public String toString()
+    {
+        String string = "";
+        for(int i = 0; i < face.length; i++)
+        {
+            for( int x = 0; x < face[i].length; x++)
+            {
+                string += face[i][x];
+            }
+            string = string + "\n";
+        }
+        return string;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -6,20 +6,24 @@ public class Runner
         JackOLanturn face1 = new JackOLanturn(face);
         face1.fill("X");
         face1.edit("", 0, 0);
-        face1.edit("", 0, 0);
-        face1.edit("", 0, 0);
-        face1.edit("", 0, 0);
+        //face1.edit("", 4, 7);
+        face1.edit("", 0, 7);
+        face1.edit("", 4, 0);
 
-        face1.edit("0", 0, 0);
-        face1.edit("0", 0, 0);
-        face1.edit("^", 0, 0);
-        face1.edit("^", 0, 0);
-        face1.edit("|", 0, 0);
-        face1.edit("|", 0, 0);
-        face1.edit("_", 0, 0);
-        face1.edit("_", 0, 0);
-        face1.edit("|", 0, 0);
-        face1.edit("|", 0, 0);
+        face1.edit("<", 1, 2);
+        face1.edit(">", 1, 5);
+        face1.edit("<", 1, 3);
+        face1.edit(">", 1, 4);
+        face1.edit("-", 2, 3);
+        face1.edit("-", 2, 4);
+        face1.edit("-", 3, 1);
+        face1.edit("|", 3, 2);
+        face1.edit("[#", 3, 3);
+        face1.edit("]", 3, 4);
+        face1.edit("|", 3, 5);
+        face1.edit("-", 3, 6);
+        face1.edit("-", 4, 4);
+        face1.edit("-", 4, 5);
         System.out.println(face1);
 
     }
